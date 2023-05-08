457f1f3f5ea5847ecfd7eaffc373fa940972705f
Aldo FWI <aldo.fwi@gmail.com>
2023-05-08 01:24:00 -0400

    Recalibrate

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -26,8 +26,11 @@ function Projects({ projects }: Props) {
         snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
             
             {projects.map((project, index) => (
-                <div key={project._id} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                    <motion.img 
+                <div 
+                    key={project._id} 
+                    className="max-h-6xl max-w-6xl flex-shrink-0 snap-center flex flex-col 
+                        space-y-5 items-center justify-center p-20 md:p-44">
+                    <motion.img
                         initial={{
                             y: -300,
                             opacity: 0,
@@ -35,7 +38,7 @@ function Projects({ projects }: Props) {
                         transition={{ duration: 1.2 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         viewport={{ once: true }}
-                        src={urlFor(project?.image).url()} alt="amazonclone" />
+                        src={urlFor(project?.image).url()} alt="" />
                     
                     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                         <h4 className='text-4xl font-semibold text-center'>
@@ -48,7 +51,7 @@ function Projects({ projects }: Props) {
                         <div className='flex items-center space-x-2 justify-center'>
                         {project?.technologies.map( technology => (
                             <img 
-                                className='h-10 w-10'
+                                className='h-10 w-10 rounded-full'
                                 key={technology._id}
                                 src={urlFor(technology.image).url()}
                                 alt=""
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -11,7 +11,7 @@ type Props = {
 function Skill({ skill, directionLeft }: Props) {
 
   return (
-    <div className="group relative flex cursor-pointer">
+    <div className="group relative flex cursor-pointer top-10">
         <motion.img
             initial={{
                 x: directionLeft ? -200 : 200,
@@ -21,10 +21,10 @@ function Skill({ skill, directionLeft }: Props) {
             whileInView={{ opacity: 1, x: 0 }}
             src={urlFor(skill?.image).url()}
             className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 
-            xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+            xl:w-30 xl:h-30 filter group-hover:grayscale transition duration-300 ease-in-out"
         />
         <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white 
-        w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 ">
+        w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
             <div className="flex items-center justify-center h-full">
                 <p className='text-3xl font-bold text-black opacity-100'>{skill?.progress}%</p>
             </div>
