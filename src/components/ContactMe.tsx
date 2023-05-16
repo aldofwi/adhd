f91bb439eaa27251941f69a78ea7fb903de0c48e
Aldo FWI <aldo.fwi@gmail.com>
2023-05-16 17:58:40 -0400

    contact fit

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -24,13 +24,13 @@ function ContactMe({}: Props) {
 
   return (
 
-    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-4xl px-10 justify-evenly mx-auto items-center">
-        <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+    <div className="h-screen w-4/5 flex relative flex-col text-center md:text-left md:flex-row max-w-2xl px-10 justify-evenly mx-auto items-center">
+        <h3 className="absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl">
             Contact
         </h3>
         
-        <div className="flex flex-col space-y-10 h-min w-min">
-            <h4 className="text-4xl font-semibold text-center">
+        <div className="absolute flex flex-col h-3/5 w-4/5 space-y-10">
+            <h4 className="text-2xl font-semibold text-center">
                 I have got just what you need.{"   "}
                 <span className='decoration-[#F7AB0A]/50 underline'>
                     Let us talk.
@@ -38,23 +38,23 @@ function ContactMe({}: Props) {
             </h4>
 
             <div>
-                <div className="flex items-center space-x-5 justify-center">
+                <div className="flex items-center space-x-3 justify-center">
                     <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
                     <p>+1234567890</p>
                 </div>
 
-                <div className="flex items-center space-x-5 justify-center">
+                <div className="flex items-center space-x-3 justify-center">
                     <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
                     <p>aldo.fwi@gmail.com</p>
                 </div>
 
-                <div className="flex items-center space-x-5 justify-center">
+                <div className="flex items-center space-x-3 justify-center">
                     <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
                     <p>345 Coding Lane</p>
                 </div>
             </div>
 
-            <form onSubmit={ handleSubmit(onSubmit) } className="flex flex-col space-y-2 w-fit mx-auto">
+            <form onSubmit={ handleSubmit(onSubmit) } className="flex flex-col space-y-2 w-min mx-auto">
                 <div className="flex space-x-2">
                     <input {...register('name')} className='contactInput' placeholder='Name' type="text" />
                     <input {...register('email')} className='contactInput' placeholder='Email' type="email" />
