a472dab516230c6c4410273da442a6d2895c4055
Aldo FWI <aldo.fwi@gmail.com>
2023-06-08 13:11:06 -0400

    About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,7 +18,7 @@ function About({ pageInfo }: Props) {
         whileInView={{ opacity: 1 }}
         transition={{ duration: 1.5 }}
         className='flex flex-col relative h-min w-3/4 text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>ABOUT</h3>
 
         <motion.img 
             initial={{
