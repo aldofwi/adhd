bd22de97572e8099989974f27e4802a14c1fec6b
Aldo FWI <aldo.fwi@gmail.com>
2023-06-22 00:57:05 -0400

    xp

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,7 @@ function Hero({ pageInfo }: Props) {
 
             <div className='pt-5'>
                 <Link href="#about"><button className='heroButton'>About</button></Link>
-                <Link href="#experience"><button className='heroButton'>XP</button></Link>
+                <Link href="#experience"><button className='heroButton'>Experience</button></Link>
                 <Link href="#skills"><button className='heroButton'>Skills</button></Link>
                 <Link href="#projects"><button className='heroButton'>Projects</button></Link>
             </div>
